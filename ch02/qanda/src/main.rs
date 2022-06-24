6d85d3401479767b4d546c2bc34120f83866bf70
Oliver Forral <intrepion@gmail.com>
2022-06-23 22:35:01 -0600

    Listing 2.1 Creating and implementing our Question type

--- ch02/qanda/src/main.rs
+++ ch02/qanda/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+ 
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
