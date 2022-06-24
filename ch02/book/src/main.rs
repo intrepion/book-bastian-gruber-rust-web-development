9d26592e61de0ba416d6b493c9e80f478e8be043
Oliver Forral <intrepion@gmail.com>
2022-06-23 23:54:28 -0600

    Listing 2.2 Using match to work with Option values

--- ch02/book/src/main.rs
+++ ch02/book/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    struct Book {
+        title: String,
+        isbn: Option<String>,
+    }
+ 
+    let book = Book { 
+        title: "Great book".to_string(), 
+        isbn: Some(String::from("1-123-456")) 
+    };
+    
+    match book.isbn {
+        Some(i) => println!("The ISBN of the book: {} is: {}", book.title, i),
+        None => println!("We don’t know the ISBN of the book"),
+    }
 }
