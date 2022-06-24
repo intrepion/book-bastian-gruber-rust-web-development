c26f20811aa8c0898b468afb4ef74e7969dbe359
Oliver Forral <intrepion@gmail.com>
2022-06-24 00:14:09 -0600

    Listing 2.3 Creating an example question and printing it

--- ch02/qanda/src/main.rs
+++ ch02/qanda/src/main.rs
@@ -1,14 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
     tags: Option<Vec<String>>,
 }
-
+ 
 struct QuestionId(String);
  
 impl Question {
@@ -20,4 +16,9 @@ impl Question {
             tags,
         }
     }
+}
+ 
+fn main() {
+    let question = Question::new("1", "First Question", "Content of question", ["faq"]);
+    println!("{}", question);
 }
