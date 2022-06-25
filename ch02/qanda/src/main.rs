59b70fc8c48a95ad7421dc06c691de6f8dc07313
Oliver Forral <intrepion@gmail.com>
2022-06-24 22:52:11 -0600

    Listing 2.10 Turning &str into String

--- ch02/qanda/src/main.rs
+++ ch02/qanda/src/main.rs
@@ -19,6 +19,11 @@ impl Question {
 }
  
 fn main() {
-    let question = Question::new("1", "First Question", "Content of question", ["faq"]);
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        ["faq".to_string()],
+    );
     println!("{}", question);
 }
