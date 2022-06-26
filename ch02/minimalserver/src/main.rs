886f7651c23d82fc7589465661f7299cfbc16fb8
Oliver Forral <intrepion@gmail.com>
2022-06-26 15:50:52 -0600

    Listing 2.22 A minimal Rust HTTP server with warp

--- ch02/minimalserver/src/main.rs
+++ ch02/minimalserver/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+ 
+#[tokio::main]
+async fn main() {
+    let hello = warp::get()
+        .map(|| format!("Hello, World!"));
+ 
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 1337))
+        .await;
 }
