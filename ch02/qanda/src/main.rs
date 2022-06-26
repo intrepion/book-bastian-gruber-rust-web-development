1598e339ee4eb6d8f52222ebddcbcca7aabcd5b5
Oliver Forral <intrepion@gmail.com>
2022-06-25 19:57:50 -0600

    Listing 2.15 Implementing the Display trait onto our Question

--- ch02/qanda/src/main.rs
+++ ch02/qanda/src/main.rs
@@ -23,6 +23,18 @@ impl std::fmt::Display for Question {
         write!(f, "{}, title: {}, content: {}, tags: {:?}", self.id, self.title, self.content, self.tags)
     }
 }
+
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
+ 
+impl std::fmt::Debug for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{:?}", self.tags)
+    }
+}
  
 fn main() {
     let question = Question::new(
