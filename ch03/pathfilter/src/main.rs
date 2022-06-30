566f4c836c16731a868c8c91b4520bc9f76c89e1
Oliver Forral <intrepion@gmail.com>
2022-06-29 21:05:42 -0600

    Listing 3.1 Starting a warp server with a routes filter object

--- ch03/pathfilter/src/main.rs
+++ ch03/pathfilter/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+// import the Filter trait from warp
+use warp::Filter;
+ 
+#[tokio::main]
+async fn main() {
+    // create a path Filter
+    let hi = warp::path("hello").map(|| format!("Hello, World!"));
+ 
+    // start the server and pass the route filter to it
+    warp::serve(hi).run(([127, 0, 0, 1], 3030)).await;
 }
