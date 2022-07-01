9a76b8b87fe7a9b354aa7c44cdbf34afe3d7fdc8
Oliver Forral <intrepion@gmail.com>
2022-06-30 22:53:16 -0600

    Listing 3.13 Prepare our application to be able to return proper cors headers

--- ch03/qanda/src/main.rs
+++ ch03/qanda/src/main.rs
@@ -1,7 +1,16 @@
 use serde::Serialize;
-use std::io::{Error, ErrorKind};
-use std::str::FromStr;
-use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use std::{
+    io::{Error, ErrorKind},
+    str::FromStr,
+};
+use warp::{
+    Filter, 
+    http::Method, 
+    reject::Reject, 
+    Rejection, 
+    Reply, 
+    http::StatusCode,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -76,13 +85,18 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
